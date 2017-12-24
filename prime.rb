9e73fc1c5dd9805bec275310a234c97ccba313a0
Avery Broderick <avery.c.broderick@gmail.com>
2017-12-24 17:17:01 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,8 @@
 def prime?(num)
-    2.upto(Math.sqrt(num).ceil) do |i|
-        break if num%i==0
-        return true if i==Math.sqrt(num).ceil
+  2..Math.sqrt(num).each do |i|
+    if num % i == 0
+      false
+    else
+      true
     end
-    return false
-end
+  end
