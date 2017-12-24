299bce42a01bdc8d60ea5adc92d2f7afdbf59f62
Avery Broderick <avery.c.broderick@gmail.com>
2017-12-24 17:27:53 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,21 +1,5 @@
-def prime?(n)
-  if n <=1
-    return false
-  end
-
-  if n == 2 || n == 3
-    return true
-  end
-
-  floor = 4
-  ceil = Math.sqrt(n).floor
-
-  (floor..ceil).each do |i|
-    if n % i == 0
-      return false
-    else
-      return true
-    end
-  end
-
+def is_prime?(num)
+  return false if num <= 1
+  Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
+  true
 end
